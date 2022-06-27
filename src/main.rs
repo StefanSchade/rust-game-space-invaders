b45313ab02343cdc203d915b9327ba36682cfba3
Dr. Stefan Schade <dr_stefan_schade@yahoo.com>
2022-06-27 14:21:30 +0200

    Game Loop and Key Check quit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 use std::error::Error;
 use std::io;
+use std::time::Duration;
+
+use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
-use crossterm::{ExecutableCommand, terminal};
+use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
 
-fn main() -> Result <(), Box<dyn Error>> {
-
+fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
 
-    audio.add("explode","explode.wav");
-    audio.add("lose","lose.wav");
-    audio.add("move","move.wav");
-    audio.add("pew","pew.wav");
-    audio.add("startup","startup.wav");
-    audio.add("win","win.wav");
+    audio.add("explode", "explode.wav");
+    audio.add("lose", "lose.wav");
+    audio.add("move", "move.wav");
+    audio.add("pew", "pew.wav");
+    audio.add("startup", "startup.wav");
+    audio.add("win", "win.wav");
 
     audio.play("startup");
 
@@ -23,6 +25,23 @@ fn main() -> Result <(), Box<dyn Error>> {
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
+
+    // Game Loop
+
+    'gameloop: loop {
+        // Input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
 
     // Cleanup
     stdout.execute(Show)?;
